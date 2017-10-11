06c60ad3ec30e03ddbde79cd4d5482d25a28ff8f
Sobolev.Ax <sobolev.ax@gmail.com>
2017-10-11 20:25:48 +0300

    start /comparison

---
+++ build/js/main.js
@@ -0,0 +1,32 @@
+
+var ourPlanetName = "Земля";
+var userName = "Петя";
+
+
+
+
+/*
+
+var admin, name;
+name = "Василий";
+admin = name;
+alert(admin);
+
+*/
+
+/*
+
+alert("Я – JavaScript!");
+
+function addScript(src){
+    var script = document.createElement('script');
+    script.src = src;
+    script.async = false; // чтобы гарантировать порядок
+    document.head.appendChild(script);
+}
+
+addScript('1.js'); // загружаться эти скрипты начнут сразу
+addScript('2.js'); // выполнятся, как только загрузятся
+addScript('3.js'); // но, гарантированно, в порядке 1 -> 2 -> 3
+
+*/
