a625b5c6757be9cf42639dfa4c48a6fe0ed681b9
sobolev-ax <sobolev.ax@gmail.com>
2017-10-12 19:06:02 +0300

    C:/Program Files/Git/logical-ops - start exercises

--- src/js/partials/first-steps.js
+++ src/js/partials/first-steps.js
@@ -1,7 +1,140 @@
 
+/* 
+var message, login;
+
+message = 
+(login == 'Вася')     ? 'Привет' :
+(login == 'Директор') ? 'Здравствуйте' :
+(login == '')         ? 'Нет логина' : 
+'';
+
+ */
+
+/* 
+result = (a + b < 4) ? 'Мало' : 'Много';
+
+ */
+/* 
+var userName = prompt("Кто пришел?", "");
+
+if (userName === null || userName === "" ) {
+  alert("Вход отменён");
+
+} else if(userName === "Админ") {
+
+  var password = prompt("Пароль?", "");
+
+  if (password === null || password === "" ) {
+    alert("Вход отменён");
+  } else if(password === "Чёрный Властелин") {
+    alert("Добро пожаловать!");
+  } else {
+    alert("Пароль неверен");
+  }
+
+} else {
+
+  alert("Я вас не знаю");
+
+}
+
+ */
+
+
+/* 
+var comparision = +prompt("Введите число", 0);
+
+if (comparision === 0) {
+  alert(0);
+} else if (comparision < 1){
+  alert(-1);
+} else if (comparision > 0){
+  alert(1);
+} 
+
+ */
+
+
+/* 
+var javaScriptName = prompt("Каково «официальное» название JavaScript?","");
+if ( javaScriptName === "ECMAScript" ) {
+  alert("Верно!");
+} else {
+  alert("Не знаете? «ECMAScript»!");
+}
+
+ */
+
+
+/* 
+
+var userName = prompt("Ваше имя?", "");
+alert(userName);
+
+ */
+
+/* 
+function isInteger(num) {
+  return (num ^ 0) === num;
+}
+
+alert( isInteger(1) ); // true
+alert( isInteger(1.5) ); // false
+alert( isInteger(-0.5) ); // false
+
+ */
+
+/*
+
+var str = "Проверка";
+
+if (~str.indexOf("верка")) { // Сочетание "if (~...indexOf)" читается как "если найдено"
+  alert( 'найдено!' );
+}
+
+*/
+
+// Округление 12.345 ^ 0
+
+/*
+
+var ACCESS_ADMIN = 1;          // 00001
+var ACCESS_GOODS_EDIT = 2;   // 00010
+var ACCESS_GOODS_VIEW = 4;     // 00100
+var ACCESS_ARTICLE_EDIT = 8; // 01000
+var ACCESS_ARTICLE_VIEW = 16;  // 10000
+
+var guest = ACCESS_ARTICLE_VIEW | ACCESS_GOODS_VIEW; // 10100
+var editor = guest | ACCESS_ARTICLE_EDIT | ACCESS_GOODS_EDIT; // 11110
+var admin = editor | ACCESS_ADMIN; // 11111
+
+alert(editor & ACCESS_ADMIN); // 0, доступа нет
+alert(editor & ACCESS_ARTICLE_EDIT); // 8, доступ есть
+
+var check = ACCESS_GOODS_VIEW | ACCESS_GOODS_EDIT; // 6, 00110
+
+alert( admin & check ); // не 0, значит есть доступ к просмотру ИЛИ изменению
+
+*/
+
+
+
+/*
+
+var access = parseInt("11000", 2);
+alert(access);
+var access2 = access.toString(2);
+alert( access2 );
+
+*/
+
+
+/*
+
 var ourPlanetName = "Земля";
 var userName = "Петя";
 
+*/
 
 
 
