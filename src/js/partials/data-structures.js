99ae390020f377ec9f9e72c72574fb270f3e2330
sobolev-ax <sobolev.ax@gmail.com>
2017-10-30 12:55:41 +0300

    start /object

--- src/js/partials/data-structures.js
+++ src/js/partials/data-structures.js
@@ -1,13 +1,152 @@
 
+function extractCurrencyValue(str) {
+  return +str.slice(1);
+}
+console.log( extractCurrencyValue("$120") );
 
-alert( getDecimal(12.345) ); // 0.345
-alert( getDecimal(1.2) ); // 0.2
-alert( getDecimal(-1.2) ); // 0.2
 
-function getDecimal(num) {
-  return num - Math.floor(num);
+
+/*
+
+function truncate(str, maxLength) {
+  var resultStr = str;
+
+  if ( str.length > maxLength ) {
+    resultStr = str.slice(0, maxLength - 3) + "...";
+  };
+
+  return resultStr;
 }
 
+console.log (
+  truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) );
+
+console.log ( truncate("Всем привет!", 20) );
+
+console.log ( truncate("What I'd like to tell on this topic is", 20) );
+
+*/
+
+/*
+
+console.log( checkSpam('buy ViAgRA now') );
+console.log( checkSpam('free xxxxx') );
+console.log( checkSpam("innocent rabbit") );
+
+function checkSpam(str) {
+  var strLoverCase = str.toLowerCase();
+  if ( ~strLoverCase.indexOf("xxx") || ~strLoverCase.indexOf("viagra") ) {
+    return true;
+  }
+  return false;
+}
+
+*/
+
+/*
+
+function ucFirst(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+*/
+
+/*
+
+setTimeout( function () {
+  console.log(getRandom(5, 10))
+}, 1000);
+setTimeout( function () {
+  console.log(getRandom(0, 5))
+}, 2000);
+setTimeout( function () {
+  console.log(getRandom(500, 1000))
+}, 3000);
+setTimeout( function () {
+  console.log(getRandom(20, 25))
+}, 4000);
+setTimeout( function () {
+  console.log(getRandom(352, 354))
+}, 5000);
+setTimeout( function () {
+  console.log(getRandom(800 ,888))
+}, 6000);
+
+function getRandom (min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  rand = Math.round(rand);
+  return rand;
+}
+
+*/
+
+
+/*
+
+setTimeout( function () {
+  console.log(getRandom(10))
+}, 1000);
+setTimeout( function () {
+  console.log(getRandom(100))
+}, 2000);
+setTimeout( function () {
+  console.log(getRandom(1000))
+}, 3000);
+setTimeout( function () {
+  console.log(getRandom(25))
+}, 4000);
+setTimeout( function () {
+  console.log(getRandom(354))
+}, 5000);
+setTimeout( function () {
+  console.log(getRandom(888))
+}, 6000);
+
+function getRandom (max) {
+  return parseInt( max * Math.random() );
+}
+
+*/
+
+/*
+
+console.log( fib(77) );
+console.log( fibBinet(77) );
+
+function fib(n) {
+  var a = 1,
+    b = 0,
+    x;
+  for (i = 0; i < n; i++) {
+    x = a + b;
+    a = b
+    b = x;
+  }
+  return b;
+}
+function fibBinet(n) {
+  var phi = (1 + Math.sqrt(5)) / 2;
+
+  return Math.pow(phi, n) / Math.sqrt(5);
+}
+
+*/
+
+
+
+/*
+console.log( getDecimal(12.345) ); // 0.345
+console.log( getDecimal(1.2) ); // 0.2
+console.log( getDecimal(-1.2) ); // 0.2
+console.log( getDecimal(1.3) ); // 0.3
+console.log( getDecimal(5) ); // 0
+
+function getDecimal(num) {
+  var numRound = Math.round(num * 1000) / 1000;
+  if (numRound < 0) numRound *= -1;
+  return Math.round( (numRound - parseInt(numRound)) * 1000 ) / 1000;
+}
+*/
 
 /*
 var numberOne = +prompt("Первое число:", "") || 0;
