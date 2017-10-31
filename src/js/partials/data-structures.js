fadaff995ba72f1155e359967ed4a02a4dab87a6
sobolev-ax <sobolev.ax@gmail.com>
2017-10-31 16:57:47 +0300

    end /array-methods

--- src/js/partials/data-structures.js
+++ src/js/partials/data-structures.js
@@ -1,5 +1,427 @@
 
 
+
+
+/*
+
+var strings = ["кришна", "кришна", "харе", "харе",
+  "харе", "харе", "кришна", "кришна", "8-()"
+];
+
+console.log( unique(strings) ); // кришна, харе, 8-()
+
+function unique(strings) {
+  var arr = [],
+      obj = {};
+
+  for (var i = 0; i < strings.length; i++) {
+    obj[ strings[i] ] = true;
+  };
+
+  for (var key in obj) {
+    arr.push( key );
+  };
+
+  return arr;
+
+};
+
+*/
+/*
+
+var arr = ["воз", "киборг", "корсет", "ЗОВ", "гробик", "костер", "сектор"];
+
+console.log( aclean(arr) );
+
+function aclean(arr) {
+  var objTemp = {},
+      arrTemp = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    var word = arr[i]
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join("");
+
+    objTemp[word] = i;
+  };
+
+  for(var key in objTemp) {
+    arrTemp.push( arr[objTemp[key]] )
+  }
+
+  arr = arrTemp;
+
+  return arr;
+};
+
+*/
+/*
+
+var list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+printList(list);
+console.log("------------------------------")
+printListRecursion(list);
+console.log("------------------------------")
+printReverseList(list);
+console.log("------------------------------")
+printReverseListRecursion(list);
+console.log("------------------------------")
+
+function printList(list) {
+  var link = list;
+
+  while (link) {
+
+    console.log(link);
+    link = link.next;
+
+  };
+
+};
+function printListRecursion(list) {
+
+  console.log(list);
+
+  if (list.next) {
+    printListRecursion(list.next)
+  };
+
+};
+function printReverseList(list) {
+  var link = list;
+  var arr = [];
+
+  while (link) {
+
+    arr.push( link );
+    link = link.next;
+
+  };
+
+  arr.reverse();
+
+  for(var i = 0; i < arr.length; i++) {
+    console.log( arr[i] );
+  };
+
+};
+function printReverseListRecursion(list) {
+
+  if (list.next) {
+
+    printReverseListRecursion(list.next)
+
+  };
+
+  console.log(list);
+
+};
+
+*/
+
+
+
+/*
+
+var vasya = { name: "Вася", age: 23 };
+var masha = { name: "Маша", age: 18 };
+var vovochka = { name: "Вовочка", age: 6 };
+
+var people = [ vasya , masha , vovochka ];
+
+people.sort(function (a, b) {
+  return a.age - b.age;
+});
+
+// теперь people: [vovochka, masha, vasya]
+console.log(people);
+
+*/
+
+
+
+/*
+
+var arr = [1, 2, 3, 4, 5];
+
+arr.sort(function (a, b) {
+
+  return Math.random() - 0.5;
+
+});
+
+console.log( arr ); // элементы в случайном порядке, например [3,5,1,2,4]
+
+*/
+
+
+/*
+
+var arr = ["HTML", "JavaScript", "CSS"];
+
+var arrSorted = arr
+  .concat()
+  .sort();
+
+console.log( ( arrSorted ) ); // CSS, HTML, JavaScript
+console.log( ( arr ) ); // HTML, JavaScript, CSS (без изменений)
+
+*/
+
+/*
+
+var arr = [5, 2, 1, -10, 8];
+
+arr.sort(function (a, b) {
+  return b - a;
+});
+
+console.log( arr );
+
+*/
+
+/*
+
+arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+console.log( arr ); // массив изменился: остались [3, 1]
+
+function filterRangeInPlace(arr, a, b) {
+
+  for(var i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= a) continue;
+
+    arr.splice(i,1);
+
+  };
+
+};
+
+*/
+
+/*
+
+var obj = {
+  className: 'menu'
+};
+
+console.log( removeClass(obj, 'open') ); // obj.className='menu'
+console.log( removeClass(obj, 'blabla') ); // без изменений (нет такого класса)
+console.log( removeClass(obj, 'menu') );
+
+function removeClass(obj, name) {
+  var arr = obj["className"].split(" ");
+
+  while (arr.indexOf(name) != -1) {
+    arr.splice(arr.indexOf(name), 1);
+  };
+
+  if ( arr.length > 1 ) {
+    obj["className"] = arr.join(" ");
+  } else {
+    obj["className"] = arr.join("");
+  }
+
+  return obj["className"];
+};
+
+*/
+
+
+/*
+
+console.log( camelize("background-color") );
+console.log( camelize("list-style-image") );
+console.log( camelize("-webkit-transition") );
+
+function camelize(str) {
+  var arr = str.split("-"),
+      result;
+
+  for (var i = 1; i < arr.length; i++) {
+    arr[i] =
+      arr[i]
+      .charAt(0)
+      .toUpperCase()
+      +
+      arr[i].slice(1)
+  };
+
+  result = arr.join("");
+
+  return result;
+}
+
+*/
+
+/*
+
+var obj = {
+  className: 'open menu'
+};
+
+addClass(obj, 'new'); // obj.className='open menu new'
+addClass(obj, 'open'); // без изменений (класс уже существует)
+addClass(obj, 'me'); // obj.className='open menu new me'
+
+console.log( obj.className ); // "open menu new me"
+
+function addClass(obj, cls) {
+  var arr = obj.className.split(' ');
+
+  if ( !!arr.indexOf(cls) ) {
+    arr.push(cls);
+
+    if (obj.className === "") {
+      obj.className = arr.join('');
+    } else {
+      obj.className = arr.join(' ');
+    }
+  };
+};
+
+*/
+
+/*
+
+console.log( getMaxSubSum([-1, 2, 3, -9]) ); //= 5
+console.log( getMaxSubSum([2, -1, 2, 3, -9]) ); //= 6
+console.log( getMaxSubSum([-1, 2, 3, -9, 11]) ); //= 11
+console.log( getMaxSubSum([-2, -1, 1, 2]) ); //= 3;
+console.log( getMaxSubSum([100, -9, 2, -3, 5]) ); //= 100
+console.log( getMaxSubSum([1, 2, 3]) ); //= 6
+
+function getMaxSubSum(arr) {
+
+  var sumMax = 0, sumCurr = 0, sumPrev = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+
+    sumCurr += arr[i];
+
+    if (sumCurr >= 0) {
+
+      sumPrev = sumCurr;
+
+    } else if (sumCurr < 0) {
+
+      sumCurr = 0;
+
+    }
+
+    if (sumPrev > sumMax) {
+      sumMax = sumPrev;
+    }
+
+  };
+
+  return sumMax;
+
+};
+
+function getMaxSubSum(arr) {
+  var maxSum = 0,
+    partialSum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    partialSum += arr[i];
+    maxSum = Math.max(maxSum, partialSum);
+    if (partialSum < 0) partialSum = 0;
+  }
+  return maxSum;
+}
+
+*/
+
+
+/*
+
+var count = 100,
+    mas   = [],
+    masPr = [],
+    sum = 0,
+    i, j, iPrime;
+
+for(i = 0; i < count; i++) {
+
+  mas.push(true);
+
+};
+
+console.log(mas);
+
+iPrime = 2;
+
+
+while( Math.pow(iPrime, 2) < count) {
+
+  for(j = 2, i = 0; i < count; i++) {
+
+    if( j * iPrime === i ) {
+
+      mas[i] = false;
+      j++;
+
+    };
+
+  };
+
+  for(i = iPrime + 1; i < count; i++) {
+
+    if( mas[i] !== false ) {
+
+      iPrime = i;
+      break;
+
+    };
+
+  };
+
+};
+
+console.log(mas);
+
+for(i = 2; i < count; i++) {
+
+  if( mas[i] !== false ) {
+
+    masPr.push( i );
+
+  };
+
+};
+
+for(i = 0; i < masPr.length; i++) {
+
+  sum += masPr[i];
+
+};
+
+console.log(masPr);
+console.log(sum);
+
+*/
+
+
+
+
+
 /*
 
 var arr = [5, 3, 8, 1];
