a7756ab4db02afe854c1270af65384e153ffd778
sobolev.ax <sobolev.ax@gmail.com>
2017-11-18 02:45:29 +0300

    start /object-conversion

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,61 @@
-console.log("hello")
+
+
+
+/* 
+var ladder = {
+  step: 0,
+  up: function() { // вверх по лестнице
+    this.step++;
+    return this;
+  },
+  down: function() { // вниз по лестнице
+    this.step--;
+    return this;
+  },
+  showStep: function() { // вывести текущую ступеньку
+    alert( this.step );
+    return this;
+  }
+};
+
+ladder.up().up().down().up().up().up().down().showStep(); // 3
+
+ */
+
+
+/* 
+var calculator = {
+  read: function() {
+    this.a = +prompt("a","");
+    this.b = +prompt("b","");
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  }
+}
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+ */
+
+/* 
+var user = {
+    name: "Вася",
+    hi: function() { alert(this.name); },
+    bye: function() { alert("Пока"); }
+  };
+  var name = "aaaaa";  
+  user.hi(); // Вася (простой вызов работает)
+
+
+  
+  // а теперь вызовем user.hi или user.bye в зависимости от имени
+  (user.name == "Вася" ? user.hi : user.bye)(); // undefined
+ */
 /*
 var a = 5;
 
