2a98b67bcce31f2c428871eb4102a05da15e5d9c
sobolev-ax <sobolev.ax@gmail.com>
2017-11-29 17:52:34 +0300

    start /constructor-new

--- src/js/partials/objects-more.js
+++ src/js/partials/objects-more.js
@@ -1,4 +1,40 @@
 
+console.log( sum(1)(2) );//== 3; // 1 + 2
+console.log( sum(1)(2)(3) );//== 6; // 1 + 2 + 3
+console.log( sum(5)(-1)(2) );//== 6
+console.log( sum(6)(-1)(-2)(-3) );//== 0
+console.log( sum(0)(1)(2)(3)(4)(5) );//== 15
+
+function sum(a) {
+  var result = a;
+
+  
+
+
+  function f(b) {
+
+      result += b;
+
+      return f;
+  };
+
+  f.valueOf = function() {
+    return result;
+  }
+  f.toString = function() {
+    return result;
+  }
+
+  return f;
+
+}
+
+
+
+/* 
+console.log( {}[0] );
+console.log( {} + {} );
+ */
 
 
 /*
