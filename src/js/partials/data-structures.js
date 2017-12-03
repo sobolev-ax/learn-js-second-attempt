6bc1261022b3e490dae549bd33900edcb12f0415
sobolev.ax <sobolev.ax@gmail.com>
2017-12-03 21:42:19 +0300

    start /js-misc

--- src/js/partials/data-structures.js
+++ src/js/partials/data-structures.js
@@ -1,4 +1,94 @@
 
+
+function f(x) {
+  return Math.random() * x; // random для удобства тестирования
+}
+
+function makeCaching(f) {
+  return function(a) {
+
+
+
+    if( !f[a] ) { // отсутствует
+
+      var result = f.call(this, a);
+
+      f[ a ] = result;
+
+      return result;
+
+    }
+
+    return f[ a ];
+
+  }
+}
+
+f = makeCaching(f);
+
+var a, b;
+
+a = f(1);
+b = f(1);
+alert( a == b ); // true (значение закешировано)
+
+b = f(2);
+alert( a == b ); // false, другой аргумент => другое значение
+
+
+
+/*
+function work(a, b) {
+  alert( a + b ); // work - произвольная функция
+}
+
+function makeLogging(f, log) {
+
+  function wrapper() {
+      log.push([].slice.call(arguments));
+      return f.apply(this, arguments);
+    }
+
+  return wrapper;
+}
+
+var log = [];
+work = makeLogging(work, log);
+
+work(1, 2); // 3
+work(4, 5); // 9
+
+for (var i = 0; i < log.length; i++) {
+  var args = log[i]; // массив из аргументов i-го вызова
+  alert( 'Лог:' + args.join() ); // "Лог:1,2", "Лог:4,5"
+}
+ */
+/* 
+function work(a) {
+   // work - произвольная функция, один аргумент
+}
+
+function makeLogging(f, log) {
+
+  function wrapper(a) {
+      log.push(a);
+      return f.call(this, a);
+    }
+
+  return wrapper;
+}
+
+var log = [];
+work = makeLogging(work, log);
+
+work(1); // 1
+work(5); // 5
+
+for (var i = 0; i < log.length; i++) {
+  alert( 'Лог:' + log[i] ); // "Лог:1", затем "Лог:5"
+}
+ */
+
 /*
 
 function formatDateString(date) {
