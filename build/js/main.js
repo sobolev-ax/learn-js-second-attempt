fc5023e6e10f56c6541b057b6be1627c99ac91f7
sobolev-ax <sobolev.ax@gmail.com>
2017-12-04 12:23:48 +0300

    start exercise on /misc-js

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,93 @@
 
 
+function formatDate(date) { 
+    var type = {}.toString.call(
+            date
+        ).slice(8, 9).toLowerCase(),
+        operations = {
+            s: function () {
+
+                var arr = date.split("-").reverse();
+
+                arr[2] = arr[2].slice(2);
+
+                return arr.join(".");
+
+            },
+            n: function() {
+
+                var objDate = new Date(date);
+
+                console.log( date )
+
+                /* return objDate.getDate() + "." + 
+                objDate.getMonth() + 1 + 
+                "." + objDate.getFullYear() */
+
+            },
+            a: function() {
+                
+            },
+            d: function() {
+                
+            },
+            o: function() {
+                return "formatDate error!"
+            }
+        }
+
+    //console.log(type)
+
+    return operations[type]();
+
+ }
+
+
+console.log( formatDate('2011-10-02') ); // 02.10.11
+console.log( formatDate(1234567890) ); // 14.02.09
+console.log( formatDate([2014, 0, 1]) ); // 01.01.14
+console.log( formatDate(new Date(2014, 0, 1)) ); // 01.01.14
+console.log( formatDate({}) ); // formatDate error!
+
+
+/* 
+function sayHi(who) {
+
+    if( who.forEach ) {
+
+        who.forEach(sayHi);
+
+    } else {
+
+        alert( "Привет: " + who );
+
+    }
+}
+ */
+
+/* 
+function getClass(obj) {
+    return {}.toString.call(obj).slice(8, -1);
+  }
+  
+  console.log( getClass(new Date) ); // Date
+  console.log( getClass([1, 2, 3]) ); // Array
+ */
+
+/* 
+console.log( typeof 1 );         // 'number'
+console.log( typeof true );      // 'boolean'
+console.log( typeof "Текст" );   // 'string'
+console.log( typeof undefined ); // 'undefined'
+console.log( typeof null );      // 'object' (ошибка в языке)
+console.log( typeof alert );     // 'function'
+
+console.log( typeof {} ); // 'object'
+console.log( typeof [] ); // 'object'
+console.log( typeof new Date ); // 'object'
+ */
+
+
 
 
 
@@ -1411,7 +1499,34 @@ console.log( counter() ); // 2, [[Scope]] -> {currentCount: 2}
 console.log( counter() ); // 3, [[Scope]] -> {currentCount: 3}
 */
 
+/* 
+function f(x) {
+  return Math.random()*x;
+}
 
+function makeCaching(f) {
+  var cache = {};
+
+  return function(x) {
+    if (!(x in cache)) {
+      cache[x] = f.call(this, x);
+    }
+    return cache[x];
+  };
+
+}
+
+f = makeCaching(f);
+
+var a = f(1);
+var b = f(1);
+alert( a == b ); // true (значение закешировано)
+
+b = f(2);
+alert( a == b ); // false, другой аргумент => другое значение
+ */
+
+/* 
 function f(x) {
   return Math.random() * x; // random для удобства тестирования
 }
@@ -1446,7 +1561,7 @@ alert( a == b ); // true (значение закешировано)
 
 b = f(2);
 alert( a == b ); // false, другой аргумент => другое значение
-
+ */
 
 
 /*
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+//= partials/js-misc.js
 //= partials/objects-more.js
 //= partials/functions-closures.js
 //= partials/data-structures.js
