4fae171475145cf31f0e7a5cb047789f9e466c84
sobolev-ax <sobolev.ax@gmail.com>
2017-12-05 12:39:25 +0300

    start exercise 3 on the /settimeout-setinterval

--- src/js/partials/js-misc.js
+++ src/js/partials/js-misc.js
@@ -1,5 +1,73 @@
 
 /* 
+printNumbersInterval();
+
+function printNumbersInterval() {
+
+  var i      = 0, 
+      finish = 20;
+
+  var timerId = setTimeout(function print() {
+
+    console.log( ++i );
+
+    if( i == finish ) {
+
+      clearTimeout( timerId );
+
+    } else {
+
+      timerId = setTimeout(print, 100);
+
+    }
+
+  }, 100);
+
+};
+ */
+
+/* 
+printNumbersInterval();
+
+function printNumbersInterval(){
+
+  var count = 20;
+
+  var timerId = setInterval(function tick(){
+
+    console.log( 20 - (--count) );
+
+    if ( !count ) {
+      clearInterval( timerId );
+    }
+
+  }, 100);
+
+};
+ */
+
+
+/* 
+// setTimeout - внутренняя ссылка исчезнет после исполнения функции
+// минимальная задержка 4s
+// setInterval - ссылка исчезнет при очистке таймера
+ */
+/* 
+//var timerId = setTimeout(func / code, delay[, arg1, arg2 ...])
+// clearTimeout(timerId);
+// Таймеры это надстройка над java script
+//-- setInterval, clearInterval()
+// Модальные окна замораживают время в chrome/safari/opera
+// setInterval не гарантирует паузы во время исполнения
+var timerId = setTimeout(function tick(){
+
+  alert("тик");
+
+  timerId = setTimeout(tick, 2000);
+
+}, 2000);
+ */
+/* 
 var leader = {
     name: "Василий Иванович"
   };
