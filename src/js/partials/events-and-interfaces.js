ef3c88d572264d4f6ebf5aacdd1596b7a866dca2
Sobolev.Ax <sobolev.ax@gmail.com>
2018-03-01 08:47:39 +0300

    exercises /introduction-browser-events

--- src/js/partials/events-and-interfaces.js
+++ src/js/partials/events-and-interfaces.js
@@ -1,1 +1,13 @@
-console.log('You are welocome!!');
+console.log('\n---------------------------------------------Задача 1\n');
+(() => {
+  const btn = document.querySelector('#input1');
+  const text = document.querySelector('#text1');
+
+  function handler() {
+    text.style.display = 'none';
+  }
+
+  if (btn !== null && text !== null) {
+    btn.addEventListener('click', handler);
+  }
+})();
