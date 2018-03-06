582ad765162c6cb8c81c8bb3ea11123cdc06ee8b
sobolev-ax <sobolev.ax@gmail.com>
2018-03-06 19:02:11 +0300

    exercise /mousemove-mouseover-mouseout-mouseenter-mouseleave

--- src/js/partials/event-details.js
+++ src/js/partials/event-details.js
@@ -1,1 +1,76 @@
-console.log('!!!!');
+console.log('\n---------------------------------------------Задача 1\n');
+(() => {
+  const elem = document.querySelector('#ul1');
+  const active = 'selected1';
+  let clicked;
+
+  if (elem === null) return;
+
+  elem.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'LI') return;
+
+    if (e.ctrlKey || e.metaKey) {
+      e.target.classList.toggle(active);
+
+      clicked = e.target;
+    } else if (e.shiftKey) {
+      const elements = e.target.parentElement.children;
+      const count = elements.length;
+      let current = 0;
+      let last = 0;
+
+      for (let i = 0; i < count; i++) {
+        if (elements[i] === e.target) current = i;
+        if (elements[i] === clicked) last = i;
+      }
+
+      const min = current < last ? current : last;
+      const max = current >= last ? current : last;
+
+      for (let i = min; i <= max; i++) {
+        elements[i].classList.add(active);
+      }
+      clicked = e.target;
+    } else {
+      const elements = e.target.parentElement.children;
+      const count = elements.length;
+
+      for (let i = 0; i < count; i++) {
+        elements[i].classList.remove(active);
+      }
+
+      e.target.classList.add(active);
+      clicked = e.target;
+    }
+  });
+})();
+console.log('\n---------------------------------------------Задача 2\n');
+(() => {
+  const elem = document.querySelector('#tree2');
+
+  if (elem === null) return;
+
+  elem.addEventListener('click', (e) => {
+    const evt = e;
+    const target = evt.target || evt.srcElement;
+
+    const node = target.getElementsByTagName('ul')[0];
+    if (!node) return; // нет детей
+
+    const span = document.createElement('span');
+    const text = target.firstChild.cloneNode(true);
+    span.innerText = target.firstChild.textContent;
+
+    target.removeChild(target.firstChild);
+    target.insertBefore(span, target.firstChild);
+
+    const elementFromPoint = document.elementFromPoint(e.clientX, e.clientY);
+    const isClickOnTitle = (elementFromPoint === span);
+
+    if (isClickOnTitle) {
+      node.style.display = node.style.display ? '' : 'none';
+    }
+    target.removeChild(span);
+    target.insertBefore(text, target.firstChild);
+  });
+})();
